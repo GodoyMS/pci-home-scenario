77681b9899bd8bc2d65596d3daea043c83bc5ba4
MCareadesoftware <programador.monstruocreativo@gmail.com>
2023-11-30 03:23:07 -0500

    Task 1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,18 @@
 import NeoGrid from "../../components/Grid/Grid";
-
+import "./Home.css"
 const Home = (): JSX.Element => {
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+    <div>
+        <div>
+            <h1 className=" title">
+
+            Near-Earth Object Overview
+            </h1>
+
+        </div>
+        <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
       <NeoGrid />
+    </div>
     </div>
   );
 };
